1a840f25b76df8c0633d212ff797324beba6485b
Sergey Okatov <svok@users.noreply.github.com>
2025-04-06 15:17:10 +0300

    M7l4 postgres

---
+++ ok-marketplace-be/ok-marketplace-repo-pgntv/README.md
@@ -0,0 +1,16 @@
+# Репозиторий на базе PostgreSQL
+Native реализация на базе библиотеки pgkn
+
+## PGKN
+
+### Установка и подготовка
+
+1. Скачиваем git-модуль:
+```bash
+git submodule install
+git submodule update
+```
+2. Устанавливаем дополнительную библиотеку
+```bash
+sudo apt install libpq-dev  
+```
--- ok-marketplace-tests/ok-marketplace-e2e-be/src/test/kotlin/docker/WiremockDockerCompose.kt
+++ ok-marketplace-tests/ok-marketplace-e2e-be/src/test/kotlin/docker/WiremockDockerCompose.kt
@@ -3,5 +3,5 @@ package ru.otus.otuskotlin.marketplace.e2e.be.docker
 import ru.otus.otuskotlin.marketplace.e2e.be.fixture.docker.AbstractDockerCompose
 
 object WiremockDockerCompose : AbstractDockerCompose(
-    "app-wiremock_1", 8080, "docker-compose-wiremock.yml"
+    "app-wiremock", 8080, "docker-compose-wiremock.yml"
 )
